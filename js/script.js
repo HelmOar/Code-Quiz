320cd87ed0108d588ef9301428bbfb8e1c7cb198
caroline <chelmore1@bigpond.com>
2023-03-04 10:47:58 +1100

    initial js added to create start of quiz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+//elements defined
+var startButton = document.querySelector("#start-button");
+var questionCard = document.querySelector("#question-card");
+var startBox = document.querySelector("#box");
+
+
+// if startQuiz button clicked
+
+var startBox = "appear"
+
+startButton.addEventListener("click", function () {
+    if (startBox === "appear") {
+        startBox = "disappear";
+        startBox.setAttribute("id", "disappear")
+    }
+});
+
+
+    //show question box
+
 //  initiate question by click on start
+
+
+
 // question toggle static /active - visable/invisable
 //question cards built into an array??
 //itirated through with it statements
@@ -6,8 +29,10 @@
 
 
 
+
 //initiate timer by click on next question or start button
 
 // questions
+//next button clicked
 
 //high band scores - local storage
