ddd23f8a7662f4f185a37a5d57831aee38b40830
caroline <chelmore1@bigpond.com>
2023-03-03 18:35:31 +1100

    array of questions created

---
+++ js/quizqs.js
@@ -0,0 +1,151 @@
+//make array of questions.
+var questions = [
+    {
+        num: 1,
+        question: "What does HTML stand for?",
+        answer: "HyperText Markup Language",
+        options: [
+            "HighText Machine Language",
+            "HyperText and links Markup Language",
+            "HyperText Markup Language",
+            "None of these"   
+         ]
+    },
+       
+    {
+        num: 2,
+        question: "What is the initial index of an Array?",
+        answer: "0",
+        options: [
+            "10",
+            "0",
+            "1",
+            "None of these"   
+                    ]
+     },
+
+     {
+        num: 3,
+        question: "Which of the following is the paragraph tag in HTML?",
+        answer: "<p>",
+        options: [
+            "<par>",
+            "<para>",
+            "<p>",
+            "None of these"   
+                    ]
+     },
+
+     {
+        num: 4,
+        question: "What is concatenation?",
+        answer: "combining strings",
+        options: [
+            "combining strings",
+            "storing strings",
+            "separating strings",
+            "printing out strings"   
+                    ]
+     },
+
+     {
+        num: 5,
+        question: "What is the property in CSS used to change the background color of an element?",
+        answer: "background-color",
+        options: [
+            "background-color",
+            "bgcolor",
+            "color",
+            "All of these"   
+                    ]
+     },
+
+     {
+        num: 6,
+        question: "In Java, what does API stand for?",
+        answer:"Applied Protocols Interface",
+        
+        options: [
+            "Applied Protocols Interface",
+            "Application Production Increments",
+            "Arrays Producing Iterations",
+            "None of these"   
+                    ]
+     },
+
+     {
+        num: 7,
+        question: "Which is the correct way to comment out something in HTML??",
+        answer: "Using <!-- and -->",
+        options: [
+            "Using ## and #",
+            "Using <!-- and -->",
+            "Using </-- and -/->",
+            "Using <!-- and -!>"   
+                    ]
+     },
+
+     {
+        num: 8,
+        question: "Which of the following is not a “primitive data” type?",
+        answer: "function",
+        options: [
+            "boolean",
+            "int",
+            "char",
+            "function"   
+                    ]
+     },
+
+     {
+        num: 9,
+        question: "What is the CSS property used to make the rounded borders or corners around an element?",
+        answer: "border-radius",
+        options: [
+            "border-collapse",
+            "border-radius",
+            "border-spacing",
+            "None of these"   
+                    ]
+     },
+
+     {
+        num: 10,
+        question: "In java script, what does json stand for?",
+        answer: "Java Script Object",
+        options: [
+            "Java Sign Number",
+            "Java Object Numerical",
+            "Java Script Object",
+            "None of these"   
+                    ]
+     },
+
+     {
+        num: 11,
+        question: "What is the correct sequence of HTML tags for starting a webpage?",
+        answer: "HTML, Head, Title, Body",
+        options: [
+            "Head, Title, HTML, Body",
+            "HTML, Body, Title, Head",
+            "HTML, Head, Title, Body",
+            "Body, HTML, Head, Title"   
+                    ]
+     },
+
+     {
+        num: 12,
+        question: "What does CSS stands for?",
+        answer: "Cascading style sheets",
+        options: [
+            "Cascade style sheets",
+            "Color and style sheets",
+            "Cascading style sheets",
+            "None of these"   
+                    ]
+     },
+    ];
+console.log(questions);
+
+
+
---
+++ js/script.js
@@ -0,0 +1,13 @@
+//  initiate question by click on start
+// question toggle static /active - visable/invisable
+//question cards built into an array??
+//itirated through with it statements
+
+
+
+
+//initiate timer by click on next question or start button
+
+// questions
+
+//high band scores - local storage
